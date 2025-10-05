9043ccaed9260941e9a4d0f9d9d974da3c945c50
Arnab Mandal <iamarnabmandal@Arnabs-MacBook-Air.local>
2025-10-05 19:56:30 +0530

    Update frontend  and server configuration

--- src/api/api.js
+++ src/api/api.js
@@ -5,12 +5,12 @@ export const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // CONTACT ROUTES
 export const CONTACT_API = {
-  SEND_EMAIL: `${VITE_API_BASE_URL}/api/contact`,
+  SEND_EMAIL: `${VITE_API_BASE_URL}/contact`,
 };
 
 // PROJECT ROUTES
 export const PROJECT_API = {
-  GET_ALL: `${VITE_API_BASE_URL}/api/projects`,
-  GET_FEATURED: `${VITE_API_BASE_URL}/api/projects/featured`,
-  CREATE: `${VITE_API_BASE_URL}/api/projects`,
+  GET_ALL: `${VITE_API_BASE_URL}/projects`,
+  GET_FEATURED: `${VITE_API_BASE_URL}/projects/featured`,
+  CREATE: `${VITE_API_BASE_URL}/projects`,
 };
